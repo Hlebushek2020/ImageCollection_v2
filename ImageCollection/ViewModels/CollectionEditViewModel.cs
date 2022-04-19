5e4146430cb970ea1b592afacb28d0d3076b8f70
Hlebushek2020 <63193749+Hlebushek2020@users.noreply.github.com>
2022-04-19 22:29:58 +0300

    Update CollectionEditViewModel.cs

--- ImageCollection/ViewModels/CollectionEditViewModel.cs
+++ ImageCollection/ViewModels/CollectionEditViewModel.cs
@@ -2,59 +2,50 @@
 using Prism.Commands;
 using System.Text.RegularExpressions;
 using System.Windows;
+using SUID = Sergey.UI.Extension.Dialogs;
 
 namespace ImageCollection.ViewModels
 {
-    internal class CollectionEditViewModel
+    internal class CollectionEditViewModel : IWindowTitle
     {
         #region Property
+        public string Title { get => App.Name; }
         public string NewName { get; set; }
         #endregion
 
         #region Commands
-        public DelegateCommand<Window> CanselCommand { get; } = new DelegateCommand<Window>((w) => w.Close());
+        public DelegateCommand<Window> CanselCommand { get; }
         public DelegateCommand<Window> OkCommand { get; }
         #endregion
 
         public CollectionEditViewModel(ICollectionsManager collectionsManager, ICollection collection)
         {
-            NewName = collection.Name;
+            if (collection != null)
+            {
+                NewName = collection.Name;
+            }
+            CanselCommand = new DelegateCommand<Window>((w) => w.Close());
             OkCommand = new DelegateCommand<Window>((w) =>
             {
                 if (string.IsNullOrWhiteSpace(NewName))
                 {
-                    return;
+                    SUID.MessageBox.Show("Название коллекции не может быть пустым!", Title, MessageBoxButton.OK, MessageBoxImage.Warning);
                 }
-                if (new Regex("[<>:\"/|?*\\\\]").IsMatch(NewName))
+                else if (new Regex("[<>:\"/|?*\\\\]").IsMatch(NewName))
                 {
-                    return;
+                    SUID.MessageBox.Show("В названии коллекции есть запрещенные символы (< > : \" / | ? * \\)!", Title, MessageBoxButton.OK, MessageBoxImage.Warning);
                 }
-                if (!collectionsManager.Rename(collection, NewName))
+                else if ((collection != null && !collectionsManager.Rename(collection, NewName)) || (collection == null && collectionsManager.Create(NewName) == null))
                 {
-                    return;
+                    SUID.MessageBox.Show("Коллекция с таким названием уже существует!", Title, MessageBoxButton.OK, MessageBoxImage.Warning);
                 }
-                w.Close();
+                else
+                {
+                    w.Close();
+                }
             });
         }
 
-        public CollectionEditViewModel(ICollectionsManager collectionsManager)
-        {
-            OkCommand = new DelegateCommand<Window>((w) =>
-            {
-                if (string.IsNullOrWhiteSpace(NewName))
-                {
-                    return;
-                }
-                if (new Regex("[<>:\"/|?*\\\\]").IsMatch(NewName))
-                {
-                    return;
-                }
-                if (collectionsManager.Create(NewName) == null)
-                {
-                    return;
-                }
-                w.Close();
-            });
-        }
+        public CollectionEditViewModel(ICollectionsManager collectionsManager) : this(collectionsManager, null) { }
     }
 }
